70fc35a1640e073bd3ab9e2c83f9593b0f50353b
markp88 <markpowers@apress.com>
2022-12-15 07:23:07 -0500

    Initial commit

---
+++ space-master/errata.md
@@ -0,0 +1,22 @@
+Technical errors:
+-----------------
+1. The description for the partitioned outer join example in Chapter 7 should be "We want to see the results for launch vehicles with at least one relevant launch in 2017, broken down into months." instead of "We want to see the results for 2017, broken down into months."  Found by Keerthi Teja Tailam.
+
+Grammatical errors:
+-------------------
+1. Change "But SQL has the huge advantage of not needed..." to "needing".
+2. PDF is missing the words "Version control" in front of "isn’t just a place to store an “official copy”" in Chapter 2.
+
+Nice-to-have features for a second edition:
+-------------------------------------------
+1. In early chapters, always add at least one blank between code lines.
+2. For the example in Chapter 6, specify that the example is best read in an IDE, not in the book.
+3. Change Hashing diagrams in Chapter 16 - use 4 diagrams instead of 3.
+4. Try Kaley's example code for hashing - maybe it does work in some cases?
+5. "TEST_FUNCTION" in Chapter 21 - add a blank line or the NULL.
+6. Change the MODEL example to avoid multiple spaces.
+7. Improve index?
+8. In sequence caching, add discussion about large batch sizes and scalable sequences reducing contention on pk index because numbers are so different.
+9. Table of contents is too large.  Is it possible to have a shorter table of contents first?  (Suggested by Nadir Doctor.)
+10. "Note that we should almost never use Oracle Express Edition".  Add sentance like "Things improved with 18c, but historically the product does not have a good track record."
+11. Version control - add note about tools like Liquibase or Flyway.  I'm not a fan, but I'm in  the minority.
---
+++ space-master/examples/Chapter 01.sql
@@ -0,0 +1,144 @@
+---------------------------------------------------------------------------
+-- SQL Alternatives
+---------------------------------------------------------------------------
+
+--Simple query used to compare with Query by Example (QBE) visual programming tool:
+select
+	satellite.norad_id,
+	satellite.launch_id,
+	launch.launch_date
+from satellite
+join launch
+	on satellite.launch_id = launch.launch_id
+where trunc(launch.launch_date) = date '1957-10-04'
+order by satellite.norad_id;
+
+
+
+---------------------------------------------------------------------------
+-- Key Concepts
+---------------------------------------------------------------------------
+
+--Oracle 21c has 2557 reserved words, whereas most programming languages only have a few dozen.
+select count(*) from v$reserved_words;
+select * from v$reserved_words;
+
+
+
+---------------------------------------------------------------------------
+-- NULL
+---------------------------------------------------------------------------
+
+--Initial SQL*Plus formatting to perfectly recreate results in book.
+--(But I hope that you're using an IDE instead of SQL*Plus.)
+set sqlprompt "SQL> ";
+alter session set current_schema = space;
+
+--Incorrect null comparisons:
+select count(*) from launch where apogee = null;
+select count(*) from launch where apogee <> null;
+
+--Correct null comparisons:
+select count(*) from launch where apogee is null;
+select count(*) from launch where apogee is not null;
+
+--Incorrect null "not in" usage:
+select count(*)
+from launch
+where launch.launch_id not in
+(
+	select satellite.launch_id
+	from satellite
+);
+
+
+
+---------------------------------------------------------------------------
+-- JOIN
+---------------------------------------------------------------------------
+
+--Inner join with "inner join" keyword:
+select *
+from launch
+inner join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--Inner join with "join" keyword:
+select *
+from launch
+join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--Inner join with Cartesian product method:
+select *
+from launch, satellite
+where launch.launch_id = satellite.launch_id;
+
+
+
+--Left join with "left outer join" keyword:
+select *
+from launch
+left outer join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--Left join with "left join" keyword:
+select *
+from launch
+left join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--Left join with "(+)" syntax:
+select *
+from launch, satellite
+where launch.launch_id = satellite.launch_id(+);
+
+
+
+--Right joins.  Not shown in the book to save space.
+select *
+from launch
+right outer join satellite
+	on launch.launch_id = satellite.launch_id;
+
+select *
+from launch
+right join satellite
+	on launch.launch_id = satellite.launch_id;
+
+select *
+from launch, satellite
+where launch.launch_id(+) = satellite.launch_id;
+
+
+
+--Full outer join with optional "outer" keyword.
+select *
+from launch
+full outer join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--Without optional "outer" keyword.
+select *
+from launch
+full join satellite
+	on launch.launch_id = satellite.launch_id;
+
+--This does not work. It raises the exception:
+--ORA-01468: a predicate may reference only one outer-joined table
+select *
+from launch, satellite
+where launch.launch_id(+) = satellite.launch_id(+);
+
+
+
+--Cross joins:
+--(These will run for a long time and return 3,040,975,455 rows)
+
+--ANSI join syntax:
+select *
+from launch
+cross join satellite;
+--Old-fashioned syntax:
+select *
+from launch, satellite;
